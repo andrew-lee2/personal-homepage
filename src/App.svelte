4182972ef22e929a44f6e187aa9b67e496383887
Andrew Lee <leeandrew2@gmail.com>
2020-06-12 14:14:06 -0500

    updated for desktop view

--- src/App.svelte
+++ src/App.svelte
@@ -5,15 +5,25 @@
 
 <main>
 <!--	TODO make some components-->
-	<BackgroundType/>
+	<div class="text-container">
+		<BackgroundType/>
+	</div>
+
+
 </main>
 
 <style>
 	main {
+		/*TODO might have to change this*/
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
+		/*max-width: 240px;*/
 		margin: 0 auto;
+		height: 100%;
+		overflow: hidden;
+		display: grid;
+		/*TODO make the grid gutter responsive*/
+		grid-template-columns: 100px auto auto 100px;
+		grid-template-rows: auto auto auto;
 	}
 
 	h1 {
@@ -21,6 +31,11 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	.text-container {
+		grid-area: 2 / 2 / 2 / 2;
+		/*justify-items: left;*/
 	}
 
 	@media (min-width: 640px) {
--- src/components/BackgroundType.svelte
+++ src/components/BackgroundType.svelte
@@ -3,30 +3,74 @@
     import { fade, fly } from 'svelte/transition';
 
     let isDone = false;
-    let cats = [
-        { id: 'J---aiyznGQ', name: 'Keyboard Cat', url: 'https://google.com' },
-        { id: 'z_AbfPXTKms', name: 'Maru', url: 'https://github.com' },
-        { id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat', url: 'https://linkedin.com' }
-    ];
 
     function handleDone() {
       isDone = true;
     }
 </script>
 
-<hr>
+<div class="site-text">
+    <!--TODO do we need to have another sentence?-->
+    <Typewriter cascade interval={100} cursor={false} on:done={handleDone}>
+        <h1>Welcome!</h1>
+    </Typewriter>
 
-<!--TODO do we need to have another sentence?-->
-<Typewriter interval={250} cursor={false} on:done={handleDone}>
-    <h1>Hello!</h1>
-</Typewriter>
+<!--    TODO dont make this a list let it fade in one at a time?-->
+    {#if isDone}
+        <div class="logo-block" in:fade="{{ duration: 2000, delay: 1000 }}" out:fade>
+<!--            TODO make a new logo that links to my about section-->
+            <a href="https://github.com/andrew-lee2">
+                <img class="logo" src="./static/images/github.svg" alt="Github logo">
+            </a>
 
-{#if isDone}
-<!--TODO make these loop components-->
-    {#each cats as { id, name, url }, i}
-        <a href={url} in:fade="{{ duration: 2000, delay: i * 200 }}" out:fade>
-            {name}
-            a
-        </a>
-    {/each}
-{/if}
+            <a href="https://github.com/andrew-lee2">
+                <img class="logo" src="./static/images/github.svg" alt="Github logo">
+            </a>
+
+            <a href="https://www.linkedin.com/in/andrewlee10/">
+                <img class="logo" src="./static/images/linkedin.svg" alt="Linkedin logo">
+            </a>
+        </div>
+    {/if}
+</div>
+
+<style>
+    /*TODO make responsive*/
+    h1 {
+        font-size: 6.5rem;
+        margin-block-end: 0;
+    }
+
+    ul {
+        list-style-type: none;
+    }
+
+
+    .site-text {
+        display: grid;
+        justify-content: center;
+    }
+
+    .logo-block {
+        display: flex;
+        justify-content: space-between;
+        /*TODO change to rem*/
+        width: 200px;
+    }
+    .logo {
+        height: 40px;
+    }
+
+    a {
+        color: black;
+    }
+
+    a:link {
+        /*TODO maybe change the color for everything*/
+        color: black;
+    }
+
+    a:hover {
+        color: red;
+    }
+</style>
