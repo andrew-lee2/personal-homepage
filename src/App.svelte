0f049333ae4f01cac085bfc594f7905111ce82c5
Andrew Lee <leeandrew2@gmail.com>
2020-06-13 12:04:23 -0500

    added routing for about page

--- src/App.svelte
+++ src/App.svelte
@@ -1,44 +1,14 @@
 <script>
-	import BackgroundType from './components/BackgroundType.svelte';
+	import { Router, Link, Route } from "svelte-routing";
+	import Home from "./pages/Home.svelte";
+	import About from "./pages/About.svelte";
+
+	export let url = "";
 </script>
 
-<main>
-	<div class="text-container">
-		<BackgroundType/>
+<Router url="{url}">
+	<div>
+		<Route path="about" component="{About}" />
+		<Route path="/"><Home /></Route>
 	</div>
-
-
-</main>
-
-<style>
-	main {
-		text-align: center;
-		margin: 0 auto;
-		height: 100%;
-		overflow: hidden;
-		display: grid;
-		grid-template-columns: 25px auto auto 25px;
-		grid-template-rows: auto auto auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	.text-container {
-		grid-area: 2 / 2 / 2 / 4;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-			grid-template-columns: 100px auto auto 100px;
-		}
-		.text-container {
-			grid-area: 2 / 2 / 2 / 2;
-		}
-	}
-</style>
+</Router>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,41 @@
+<script>
+    import BackgroundType from '../components/BackgroundType.svelte';
+</script>
+
+<main>
+    <div class="text-container">
+        <BackgroundType/>
+    </div>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        margin: 0 auto;
+        height: 100%;
+        overflow: hidden;
+        display: grid;
+        grid-template-columns: 25px auto auto 25px;
+        grid-template-rows: auto auto auto;
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    .text-container {
+        grid-area: 2 / 2 / 2 / 4;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            grid-template-columns: 100px auto auto 100px;
+        }
+        .text-container {
+            grid-area: 2 / 2 / 2 / 2;
+        }
+    }
+</style>
