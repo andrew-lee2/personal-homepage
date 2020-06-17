2f36ff796a1f6138808a6b53658ec2eb4b6ed112
Andrew Lee <leeandrew2@gmail.com>
2020-06-17 09:00:10 -0500

    WIP added logo and favicon need to style navbar

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,8 +1,33 @@
 <script>
     import BackgroundType from '../components/BackgroundType.svelte';
+    let navClicked = false;
+
+    function handleClick() {
+        navClicked = !navClicked;
+        console.log(navClicked);
+    }
 </script>
 
 <main>
+<!--    TODO make own component eventually-->
+    <div class="logo-container">
+        <img
+                on:click={handleClick}
+                class="logo"
+                src="./static/images/logo.svg"
+                alt="logo for website"
+        />
+    </div>
+<!--    TODO need to make a navbar that becomes visable on click of logo-->
+    <div class="navbar">
+<!--        TODO idk if we want to duplicate this-->
+        <img class="logo"
+             src="./static/images/logo.svg"
+             alt="logo for website"/>
+        <p>
+            <a href="/about">About</a>
+        </p>
+    </div>
     <div class="text-container">
         <BackgroundType/>
     </div>
@@ -29,7 +54,18 @@
     .text-container {
         grid-area: 2 / 2 / 2 / 4;
     }
-
+    .logo-container {
+        grid-area: 1 / 1 / 1 / 1;
+    }
+    .logo {
+        height: 45px;
+        width: 45px;
+    }
+    .navbar {
+        grid-area: 1 / 1 / 4 / 1;
+        /*TODO choose different color*/
+        background-color: grey;
+    }
     @media (min-width: 640px) {
         main {
             grid-template-columns: 100px auto auto 100px;
