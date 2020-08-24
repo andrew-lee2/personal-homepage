72ec8e367cae4088f03fea7814f1f77ffa09fabb
Andrew Lee <leeandrew2@gmail.com>
2020-08-23 20:11:21 -0500

    WIP split out to basepage

--- src/components/BasePage.svelte
+++ src/components/BasePage.svelte
@@ -9,9 +9,27 @@
     <Sidebar bind:open/>
     <Navbar bind:sidebar={open}/>
     
-    <main>
-        <div class="container">
-            <div bind:this="{textElement}" class="text"></div>
-        </div>
-    </main>
 </div>
+
+<style type="text/scss">
+    @import '../styles/vars';
+    // TODO look at using a different font-family?
+    // TODO this should be temporary til we fix sidebar
+    .fix-home {
+        position: fixed;
+        width: 100%;
+        height: 100%;
+    }
+    main {
+        height: 100%;
+        width: 100%;
+        background-color: $primary-base;
+		transition: background-color 0.3s;
+    }
+
+	:global(body.dark-mode)  {
+        & main {
+		    background-color: $dark-base-dark-grey;
+        }
+	}
+</style>
