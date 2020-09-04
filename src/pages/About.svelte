a3995e2ab9148f1dae7360cfbf1e925317db8300
Andrew Lee <leeandrew2@gmail.com>
2020-09-04 07:42:01 -0500

    Finished about page

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,14 +1,54 @@
 <script lang="ts">
+    import SocialLinks from '../components/SocialLinks.svelte';
     // https://github.com/EmilTholin/svelte-routing/issues/122
     export let location: any;
+    const src: string = '../static/images/about_profile_pic2.jpg';
 </script>
 
 <main>
-    <p>Page under construction</p>
+    <div class="container">
+        <div class="profile-pic-container">
+            <img {src} alt="Profile picture for about page">
+        </div>
+        <div class="bio-container">
+            <p>
+                Hello! I’m Andrew. I am currently a software engineer at Indeed on the Hiring Events team. Previously I worked in analytics at Indeed and Teradata.
+            </p>
+            <p>MS in Industrial Engineering from NC State</p>
+            <p>BS in Math from Clemson</p>
+            <p>Outside of work I enjoy traveling, tennis, board games, swimming, cooking, sports analytics, and watching dramas.</p>
+        </div>
+        <div class="footer">
+            <SocialLinks></SocialLinks>
+        </div>
+    </div>
 </main>
 
 <style type="text/scss">
     @import '../styles/vars';
+    .footer {
+        margin: 1rem 0 4rem 0;
+    }
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-content: center;
+        width: 60%;
+        margin: 4rem auto 0 auto;
+        @media screen and (min-width: $small) {
+            width: 25%;
+        }
+    }
+    .profile-pic-container {
+        margin: 1rem 0;
+        display: flex;
+        justify-content: center;
+    }
+    img {
+        max-width: 100%;
+        max-height: 100%;
+    }
     main {
         height: 100%;
         width: 100%;
@@ -17,16 +57,15 @@
     }
 
     p {
-        height: 65%;
-        width: 100%;
-        justify-content: center;
-        align-items: center;
-        display: flex;
+        color: $primary-text-color;
     }
 
     :global(body.dark-mode)  {
         & main {
 		    background-color: $dark-base-dark-grey;
+        }
+        p {
+            color: $dark-text-color;
         }
     }
 </style>
